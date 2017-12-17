f582a2653a9ef99fefce120cb1826121d1c10827
Gary Danko <gary_danko@intuit.com>
2017-12-16 20:47:39 -0800

    update other device modules to match dimmer:

--- iris/devices/doorlock.py
+++ iris/devices/doorlock.py
@@ -16,6 +16,18 @@ class DoorLock(Device):
 		writable = utils.fetch_writable_attributes(readable)
 		methods = utils.fetch_methods(self.namespace, self.iris.validator)
 
+		def generate_get_fn(namespace, attribute, obj):
+			fn_name = "get_{}_{}".format(namespace, attribute)
+			def fn(self, **kwargs):
+				request.get_attributes(client=self, namespace=namespace, method=fn_name, attribute=attribute, settngs=obj, **kwargs)
+			setattr(self.__class__, fn_name, fn)
+
+		def generate_set_fn(namespace, attribute, obj):
+			fn_name = "set_{}_{}".format(namespace, attribute)
+			def fn(self, **kwargs):
+				request.set_attributes(client=self, namespace=namespace, method=fn_name, attribute=attribute, settings=obj, **kwargs)
+			setattr(self.__class__, fn_name, fn)
+
 		def generate_method_fn(method, enabled, required, oneof, valid):
 			if enabled == True:
 				fn_name = method
@@ -23,5 +35,14 @@ class DoorLock(Device):
 					request.device_method_request(client=self, namespace=self.namespace, method=method, required=required, oneof=oneof, valid=valid, **kwargs)
 				setattr(self.__class__, fn_name, fn)
 
-		for method_name, obj in methods.items():
-			generate_method_fn(method_name, obj["enabled"], obj["required"], obj["oneof"], obj["valid"])		
+		if self.iris.validator[self.namespace]["is_device"] == True:
+			for namespace in readable.keys():
+				for attribute, obj in readable[namespace].items():
+					generate_get_fn(namespace, attribute, obj)
+
+			for namespace in writable.keys():
+				for attribute, obj in writable[namespace].items():
+					generate_set_fn(namespace, attribute, obj)
+
+			for method_name, obj in methods.items():
+				generate_method_fn(method_name, obj["enabled"], obj["required"], obj["oneof"], obj["valid"])		
