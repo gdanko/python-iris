a91b077eea82898347785c62d1199bfa557acf80
Gary Danko <gary_danko@intuit.com>
2017-12-17 20:29:46 -0800

    add camera and cameraptx devices; add security alarm mode subsystem

---
+++ iris/devices/camera.py
@@ -0,0 +1,48 @@
+import iris.payloads as payloads
+import iris.request as request
+import iris.utils as utils
+from iris.capabilities.device import Device
+from pprint import pprint
+
+class Camera(Device):
+	def __init__(self, **kwargs):
+		Device.__init__(self, **kwargs)
+		self.namespace = "camera"
+		self.device_type = "Camera"
+
+		module_capabilities = ["ccamera"]
+		capabilities = sorted(module_capabilities)
+		readable = utils.fetch_readable_attributes(self.iris.validator, capabilities)
+		writable = utils.fetch_writable_attributes(readable)
+		methods = utils.fetch_methods(self.namespace, self.iris.validator)
+
+		def generate_get_fn(namespace, attribute, obj):
+			fn_name = "get_{}_{}".format(namespace, attribute)
+			def fn(self, **kwargs):
+				request.get_attributes(client=self, namespace=namespace, method=fn_name, attribute=attribute, settngs=obj, **kwargs)
+			setattr(self.__class__, fn_name, fn)
+
+		def generate_set_fn(namespace, attribute, obj):
+			fn_name = "set_{}_{}".format(namespace, attribute)
+			def fn(self, **kwargs):
+				request.set_attributes(client=self, namespace=namespace, method=fn_name, attribute=attribute, settings=obj, **kwargs)
+			setattr(self.__class__, fn_name, fn)
+
+		def generate_method_fn(method, enabled, required, oneof, valid):
+			if enabled == True:
+				fn_name = method
+				def fn(self, **kwargs):
+					request.device_method_request(client=self, namespace=self.namespace, method=method, required=required, oneof=oneof, valid=valid, **kwargs)
+				setattr(self.__class__, fn_name, fn)
+
+		if self.iris.validator[self.namespace]["is_device"] == True:
+			for namespace in readable.keys():
+				for attribute, obj in readable[namespace].items():
+					generate_get_fn(namespace, attribute, obj)
+
+			for namespace in writable.keys():
+				for attribute, obj in writable[namespace].items():
+					generate_set_fn(namespace, attribute, obj)
+
+			for method_name, obj in methods.items():
+				generate_method_fn(method_name, obj["enabled"], obj["required"], obj["oneof"], obj["valid"])
