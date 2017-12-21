880dec2b6f11e2de1dfffe65f3ccea4558db3203
Gary Danko <gary_danko@intuit.com>
2017-12-20 19:31:56 -0800

    add 1st and 2nd gen smartplugs

---
+++ iris/devices/smartplug2g.py
@@ -0,0 +1,25 @@
+import iris.utils as utils
+from iris.capabilities.device import Device
+
+class SmartPlug2G(Device):
+	def __init__(self, **kwargs):
+		Device.__init__(self, **kwargs)
+		self.namespace = "centralitesmartplug"
+		self.device_type = "SmartPlug"
+
+		module_capabilities = ["centralitesmartplug", "devota", "ident", "pow", "swit"]
+		capabilities = sorted(module_capabilities)
+		readable = utils.fetch_readable_attributes(self.iris.validator, capabilities)
+		writable = utils.fetch_writable_attributes(readable)
+		methods = utils.fetch_methods(self.namespace, self.iris.validator)
+
+		def generate_method_fn(method, enabled, required, oneof, valid):
+			if enabled == True:
+				fn_name = method
+				def fn(self, **kwargs):
+					request.device_method_request(client=self, namespace=self.namespace, method=method, required=required, oneof=oneof, valid=valid, **kwargs)
+				setattr(self.__class__, fn_name, fn)
+
+		if self.iris.validator[self.namespace]["is_device"] == True:
+			for method_name, obj in methods.items():
+				generate_method_fn(method_name, obj["enabled"], obj["required"], obj["oneof"], obj["valid"])
